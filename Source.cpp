5a59e63541362925d1cce31fc4116cece1f9bb15
AmiraFawzyMohsen <80916085+AmiraFawzyMohsen@users.noreply.github.com>
2021-04-23 22:05:00 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,30 +1,10 @@
-import java.util.*;
-public class Test{
-public static void main(String[] args) {
-		// Create a Scanner
-		Scanner input = new Scanner(System.in);
-
-		// Invoke getArray method
-		int[] array = getArray();
-
-		// Prompt the user to enter the index of the array
-		System.out.print("Enter the index of the array: ");
-		try {
-			// Display the corresponding element value
-			System.out.println("The corresponding element value is " + 
-				array[input.nextInt()]);
-		}
-		catch (ArrayIndexOutOfBoundsException ex) {
-			System.out.println("Out of Bounds.");
-		}
+Class Tiger extends Animal{
+	public abstract String sound(){
+		return "Tiger: RROOAARR";
 	}
-
-	/** Returns an array with 100 randomly chosen integers */
-	public static int[] getArray() {
-		int[] array = new int[100];
-		for (int i = 0; i < array.length; i++) {
-			array[i] = (int)(Math.random() * 100) + 1;
-		}
-		return array;
+Class Chickenextends Animal implements Edible{
+	public abstract String howToEat(){
+		return "Chicken: cock-a-doodle-doo";
+		public abstract String sound(){
+		return "Tiger: RROOAARR";
 	}
-}
